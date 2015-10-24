b517af630f5c6ae6b4cf3be9a243903a76d5cf8b
root <root@ip-10-0-1-181.us-west-2.compute.internal>
2015-10-24 18:45:24 +0000

    templated patient home-page 	modified:   cgi-bin/patient.pl 	modified:   hbs/patient.hbs

--- cgi-bin/patient.pl
+++ cgi-bin/patient.pl
@@ -10,5 +10,20 @@ my $query = CGI->new;
 my $id = $query->param('id') || 'unknown';
 
 use Text::Handlebars;
+use File::Slurp;
 
-print sprintf('stand-in for patient id [%s]', $id);
+my $patient_file = sprintf('%s/hbs/patient.hbs', $ENV{DOCUMENT_ROOT});
+
+my $hbstemplate = File::Slurp::read_file($patient_file);
+
+my $patientdata = {
+	patientname => 'patient name',
+	patientid => $id,
+	pcp => 'pcp here',
+	hpi => 'hpi here',
+	allergies => 'allergies here',
+	dischargeinstructions => 'discharge instructions here',
+	meds => ['med1', 'med2', 'med3'],
+};
+
+print Text::Handlebars->new->render_string($hbstemplate, $patientdata);
